38fe91f3ea984182aefbcf19cb07b5d3723d37ef
CindyCarolline <cindycarolline673@gmail.com>
2025-12-29 17:31:24 +0700

    fix db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 // db.js
 const { Pool } = require('pg');
 
+// Gunakan connectionString agar otomatis membaca DATABASE_URL dari Vercel
 const pool = new Pool({
-  user: 'postgres',           // ganti sesuai user PostgreSQL kamu
-  host: 'localhost',          // bisa juga IP/host remote
-  database: 'db_skripsi',  // ganti dengan nama database kamu
-  password: 'psppi',  // ganti dengan password kamu
-  port: 5432,                 // default PostgreSQL port
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false // Wajib diaktifkan agar bisa konek ke Supabase/Cloud DB
+  }
 });
 
 module.exports = pool;
