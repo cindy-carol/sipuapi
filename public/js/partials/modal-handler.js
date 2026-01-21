72fb8e9bbe857b6cf419ba883a04bcb664a7cca3
CindyCarolline <cindycarolline673@gmail.com>
2026-01-21 21:26:18 +0700

    merapihkan controller model dan middleware

--- public/js/partials/modal-handler.js
+++ public/js/partials/modal-handler.js
@@ -291,7 +291,7 @@ window.toggleTempatEdit = function(selectEl) {
                 tempatInput.value = 'Ruang Sidang PSPPI, Gedung A FT Universitas Lampung';
             }
         } else {
-            tempatInput.value = 'Ruang Sidang PSPPI, Gedung A FT Universitas Lampung'; 
+            tempatInput.value = 'Online'; 
         }
     }
 };
